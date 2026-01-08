347ec57f08fce03da9e890eb1ed4a83c63dc6181
aysenurulkucreates <aysenurulku17@gmail.com>
2026-01-08 15:17:03 +0300

    Javascript logic in components

--- PartOne/src/main.jsx
+++ PartOne/src/main.jsx
@@ -75,6 +75,12 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
   return (
     <footer>{new Date().toLocaleTimeString()}. We're currently open!</footer>
   );
