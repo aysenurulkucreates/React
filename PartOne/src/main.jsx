8510982d759b3da5608434521d36e3fc23045fb6
aysenurulkucreates <aysenurulku17@gmail.com>
2026-01-11 13:18:06 +0300

    React fragments

--- PartOne/src/main.jsx
+++ PartOne/src/main.jsx
@@ -76,11 +76,17 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <React.Fragment>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </React.Fragment>
       ) : (
         <p>We're working on our menu. Please come back later :)</p>
       )}
