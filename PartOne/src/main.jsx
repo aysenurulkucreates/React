1a4f1bad2a7672fc0fc550f75ff3998f1b11a1f5
aysenurulkucreates <aysenurulku17@gmail.com>
2026-01-11 12:32:46 +0300

    Conditional rendering with ternaries

--- PartOne/src/main.jsx
+++ PartOne/src/main.jsx
@@ -75,12 +75,14 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -108,11 +110,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
