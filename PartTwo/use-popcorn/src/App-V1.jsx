2235ffad9332555b0643980a9d64651954301392
aysenurulkucreates <aysenurulku17@gmail.com>
2026-01-27 19:50:45 +0300

    Adding a loading state & using useEffect

--- PartTwo/use-popcorn/src/App-V1.jsx
+++ PartTwo/use-popcorn/src/App-V1.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,26 +50,9 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "24d1ddb6";
-
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const query = "interstellar";
-
-  useEffect(function () {
-    async function fetchMovies() {
-      setIsLoading(true);
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-      );
-      const data = await res.json();
-      setMovies(data.Search);
-      setIsLoading(false);
-    }
-    fetchMovies();
-  }, []);
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -80,7 +63,7 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box element={isLoading ? <Loader /> : <MovieList movies={movies} />} />
+        <Box element={<MovieList movies={movies} />} />
         <Box
           element={
             <>
@@ -92,9 +75,6 @@ export default function App() {
       </Main>
     </>
   );
-}
-function Loader() {
-  return <p className="loader">Loading...</p>;
 }
 
 function NavBar({ children }) {
