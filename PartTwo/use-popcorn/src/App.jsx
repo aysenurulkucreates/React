249e415f516a34cbea1bd8431ca6f178327a2c11
aysenurulkucreates <aysenurulku17@gmail.com>
2026-01-31 17:31:41 +0300

    Refs to Select DOM Elements

--- PartTwo/use-popcorn/src/App.jsx
+++ PartTwo/use-popcorn/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import StarRating from "./starRating";
 
 const average = (arr) =>
@@ -156,6 +156,23 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery],
+  );
+
   return (
     <input
       className="search"
@@ -163,6 +180,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
