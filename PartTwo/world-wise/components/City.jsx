f5d1754c568475b69e4dc64a051854342e39e4ed
aysenurulkucreates <aysenurulku17@gmail.com>
2026-02-05 12:58:07 +0300

    Reading and setting a query string

--- PartTwo/world-wise/components/City.jsx
+++ PartTwo/world-wise/components/City.jsx
@@ -1,4 +1,4 @@
-import { userParams } from "react-router-dom";
+import { userParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
 
 const formatDate = (date) =>
@@ -11,6 +11,9 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = userParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   // TEMP DATA
   const currentCity = {
